21ac80a542701e3563e4e7d5f5a96ccf742fa891
yoongles7 <pritikas820@gmail.com>
2025-10-28 21:39:47 +0545

    pyqt5

---
+++ advanced_notes/pyqt5/intro.py
@@ -0,0 +1,22 @@
+# PyQt5 Introduction
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QIcon
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("PyQt5 GUI")
+        self.setGeometry(700, 300, 200, 200)
+        self.setWindowIcon(QIcon("yoongles.jpg"))
+    
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+    
+
+if __name__ == "__main__":
+    main()
