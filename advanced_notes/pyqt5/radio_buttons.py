426dbd936efa94a46483dd8b544c56f5d053c8a0
yoongles7 <pritikas820@gmail.com>
2025-11-04 22:50:25 +0545

    radio buttons

---
+++ advanced_notes/pyqt5/radio_buttons.py
@@ -0,0 +1,43 @@
+# PyQt5 Radio Buttons
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QRadioButton, QButtonGroup
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(700, 300, 500, 500)
+        self.radio1 = QRadioButton("Visa", self)
+        self.radio2 = QRadioButton("Mastercard", self)
+        self.radio3 = QRadioButton("Gift Card", self)
+        self.radio4 = QRadioButton("In-store", self)
+        self.radio5 = QRadioButton("Online", self)
+        self.button_group1 = QButtonGroup(self)
+        self.button_group2 = QButtonGroup(self)
+        self.initUI()
+        
+    def initUI(self):
+        self.radio1.setGeometry(0, 0, 300, 50)
+        self.radio2.setGeometry(0, 50, 300, 50)
+        self.radio3.setGeometry(0, 100, 300, 50)
+        self.radio4.setGeometry(0, 200, 300, 50)
+        self.radio5.setGeometry(0, 250, 300, 50)
+        
+        self.setStyleSheet("QRadioButton{"
+                           "font-size: 40px;" 
+                           "font-family: Arial;" 
+                           "padding: 10px"
+                           "}")
+        
+        self.button_group1.addButton(self.radio1)
+        self.button_group1.addButton(self.radio2)
+        self.button_group1.addButton(self.radio3)
+        self.button_group2.addButton(self.radio4)
+        self.button_group2.addButton(self.radio5)
+    
+    
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
